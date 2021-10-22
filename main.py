0e6385e4f7d14ed12f41df7ddebb2607426eba7e
DW1998 <Daniel.Worringer@t-online.de>
2021-10-22 18:24:28 +0200

    added cur_id for triples

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from PIL import Image
+
 import nnhash
 
 from client import Client
 from server import Server
 
+cur_id = 0
 
 class Triple:
     def __init__(self, y, id, ad):
@@ -10,6 +13,10 @@ class Triple:
         self.id = id
         self.ad = ad
 
+def get_id():
+    global cur_id
+    cur_id = cur_id + 1
+    return cur_id
 
 if __name__ == '__main__':
     server = Server("Apple")
@@ -20,4 +27,8 @@ if __name__ == '__main__':
     #c1.send_voucher(str(500))
 
     hash_val = nnhash.run("dog.png")
+    triple1 = Triple(nnhash.run("dog.png"), get_id(), Image.open("dog.png"))
+    triple2 = Triple(nnhash.run("dog.png"), get_id(), Image.open("dog.png"))
+    print(triple1.id)
+    print(triple2.id)
     print(hash_val)
