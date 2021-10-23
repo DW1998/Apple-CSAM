1e1a96b5c2545d947f722b2a44a14274d44bd28d
daniel.worringer <Daniel.Worringer@t-online.de>
2021-10-23 16:53:36 +0200

    added image viewer gui

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 import nnhash
 import PySimpleGUI as sg
+import os.path
 
 from client import Client
 from server import Server
@@ -21,6 +22,7 @@ def get_id():
     cur_id = cur_id + 1
     return cur_id
 
+
 def get_input():
     client_id = int(input("Enter next client id"))
     pic = input("Enter next picture")
@@ -29,14 +31,71 @@ def get_input():
     server.client_list[index].add_triple(triple3)
 
 
-layout = [[sg.Text("GUI test")], [sg.Button("OK")]]
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+]
 
-window = sg.Window("Demo", layout)
+# For now will only show the name of the file that was chosen
+
+image_viewer_column = [
+    [sg.Text("Choose an image from list on left:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
+]
+
+# ----- Full layout -----
+
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
+
+window = sg.Window("Image Viewer", layout)
+
+# Run the Event Loop
 
 while True:
     event, values = window.read()
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == "Exit" or event == sg.WIN_CLOSED:
         break
+    # Folder name was filled in, make a list of files in the folder
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".png", ".gif"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    elif event == "-FILE LIST-":  # A file was chosen from the listbox
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+            window["-TOUT-"].update(filename)
+            window["-IMAGE-"].update(filename=filename)
+
+        except:
+            pass
 
 window.close()
 
@@ -44,12 +103,6 @@ if __name__ == '__main__':
     server = Server("Apple")
     c1 = Client(1, server)
     c2 = Client(2, server)
-
-    while True:
-        get_input()
-        stop = input("end input? Y or N")
-        if (stop == "Y"):
-            break
 
     hash_val = nnhash.run("dog.png")
     triple1 = Triple(nnhash.run("dog.png"), get_id(), Image.open("dog.png"))
