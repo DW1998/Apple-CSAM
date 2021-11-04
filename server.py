40bdae967da8b7f9ae8cc6d19a403f88fa568ba4
daniel.worringer <Daniel.Worringer@t-online.de>
2021-11-04 18:48:11 +0100

    small changes

--- server.py
+++ server.py
@@ -160,8 +160,6 @@ class Server:
                 S2 = self.alpha * Q2
                 rkey1 = util.aes128_dec(util.calc_H_dash(S1), v.ct1)
                 rkey2 = util.aes128_dec(util.calc_H_dash(S2), v.ct2)
-                print(rkey1)
-                print(rkey2)
                 rct_dec = None
                 if rkey1 is None and rkey2 is None:
                     print("voucher does not match")
